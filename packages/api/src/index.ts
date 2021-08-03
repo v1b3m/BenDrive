8cc221b4323ca73781220f9304b9c32732a3baca
Benjamin Mayanja <vibenjamin6@gmail.com>
2021-08-03 12:43:22 +0300

    feat(api): add middleware to backend

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { schema } from "./schema";
 import { listing } from "./utils";
+import { middleware } from "./init/middleware";
 
 export const root = {
   hello: (): string => "Hello world!",
@@ -9,6 +10,8 @@ export const root = {
 };
 
 const app = express();
+middleware(app);
+
 app.use("/graphql", graphqlHTTP({ schema, rootValue: root, graphiql: true }));
 app.listen(4000, () => {
   console.log("Listening on http://localhost:4000/graphql");
