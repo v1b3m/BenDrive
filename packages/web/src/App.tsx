5092f42e894fffa3301cfa0b7834334109137bf4
Benjamin Mayanja <vibenjamin6@gmail.com>
2021-08-03 12:39:38 +0300

    feat(web): add graphql-request

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Box } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import { client } from "./requests";
+import { gql } from "graphql-request";
+
+const query = gql`
+  query Hola {
+    hello
+  }
+`;
 
 function App(): JSX.Element {
+  const [listing, setListing] = useState([]);
+
+  useEffect(() => {
+    client.request(query).then(console.log);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Header />
+      <Box p="7rem 2rem">Hey</Box>
+    </Box>
   );
 }
 
---
+++ packages/web/src/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    imperialRed: "#e63946",
+    honeyDew: "#f1faee",
+    powderBlue: "#a8dadc",
+    celadonBlue: "#457b9d",
+    prussianBlue: "#1d3557"
+  }
+};
+
+export const theme = extendTheme({ colors });
